da0b48cd47273962f8752c0b32947376049d7ea2
Nikola Malinovic <nikolamalinovic42@gmail.com>
2024-02-17 20:30:53 +0100

    Add locators

---
+++ src/contentScript/locators.ts
@@ -0,0 +1,7 @@
+export function isOnSc(origin: string): boolean {
+  return origin.includes('soundcloud');
+}
+
+export function isOnHypeddit(origin: string): boolean {
+  return origin.includes('hypeddit');
+}
