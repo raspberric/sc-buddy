02b898adadaed40c080c973dd53361eca6bed6d8
Nikola Malinovic <nikolamalinovic42@gmail.com>
2024-02-17 15:39:50 +0100

    Handle playlists in contentScript

--- src/contentScript/index.ts
+++ src/contentScript/index.ts
@@ -1,1 +1,30 @@
-console.info('contentScript is running')
+import { UnrecoverableError } from '../utils/error';
+import { PlaylistsHandler } from './playlistsHandler';
+
+const pathName = window.location.pathname;
+
+try {
+  if (pathName === '/you/sets') {
+    preparePlaylists();
+  }
+} catch (e) {
+  console.error(e);
+}
+
+async function preparePlaylists() {
+  const content = document.querySelector('#content');
+  if (!content) {
+    throw new UnrecoverableError(
+      'No content found! This might be a change on SC end, check content selector',
+    );
+  }
+
+  const playlistsHandler = new PlaylistsHandler(content);
+  const playlistItems = await playlistsHandler.loadPlaylistItems();
+
+  if (!playlistItems) {
+    throw new UnrecoverableError('No playlists found!');
+  }
+
+  await playlistsHandler.saveToStorage();
+}
