6e2417c2aa73db67640c360cec2dcdb77045065a
Nikola Malinovic <nikolamalinovic42@gmail.com>
2024-02-19 00:33:55 +0100

    Add hypeddit handling to contentScript

--- src/contentScript/index.ts
+++ src/contentScript/index.ts
@@ -1,18 +1,56 @@
 import { UnrecoverableError } from '../utils/error';
+import { Waiter } from '../utils/waiter';
+import { HypedditHandler } from './hypedditHandler';
+import { isOnHypeddit, isOnSc, isOnSecureSc } from './locators';
 import { PlaylistsHandler } from './playlistsHandler';
+import { TrackHandler } from './trackHandler';
 
 const pathName = window.location.pathname;
+const origin = window.location.origin;
 
 try {
-  if (pathName === '/you/sets') {
-    preparePlaylists();
+  if (isOnSc(origin)) {
+    if (pathName === '/you/sets') {
+      // preparePlaylists(document.querySelector('#content'));
+    } else {
+      // downloadTrack(document.querySelector('#content'));
+    }
+  }
+
+  if (isOnHypeddit(origin)) {
+    downloadFromHypeddit(document.querySelector('.release-content'));
+  }
+
+  if (isOnSecureSc(origin)) {
+    authorizeSc();
   }
 } catch (e) {
   console.error(e);
 }
 
-async function preparePlaylists() {
-  const content = document.querySelector('#content');
+async function authorizeSc() {
+  const container = document.querySelector('#app');
+  const waiter = new Waiter();
+  const button = await waiter.waitForElement(container!, '#submit_signup');
+
+  if (!button) {
+    throw new UnrecoverableError('No continue button on SC secure!');
+  }
+  (button as HTMLButtonElement).click();
+}
+
+function downloadFromHypeddit(content: HTMLElement | null) {
+  if (!content) {
+    throw new UnrecoverableError(
+      'No content found! This might be a change on SC end, check content selector',
+    );
+  }
+
+  const hypedditHandler = new HypedditHandler(content);
+  hypedditHandler.downloadTrack();
+}
+
+async function preparePlaylists(content: Element | null) {
   if (!content) {
     throw new UnrecoverableError(
       'No content found! This might be a change on SC end, check content selector',
@@ -25,6 +63,17 @@ async function preparePlaylists() {
   if (!playlistItems) {
     throw new UnrecoverableError('No playlists found!');
   }
+  // await playlistsHandler.saveToStorage();
+  playlistsHandler.getPlaylistItemsUrls(playlistsHandler.playlistItems[4].path);
+}
 
-  await playlistsHandler.saveToStorage();
+async function downloadTrack(content: Element | null) {
+  if (!content) {
+    throw new UnrecoverableError(
+      'No content found! This might be a change on SC end, check content selector',
+    );
+  }
+
+  const trackHandler = new TrackHandler(content);
+  trackHandler.downloadTrack();
 }
